31e21c8e535c01984d3a59ff6b3457cff372bb11
vero9003 <Roberto.VeigaBorneo@nielsen.com>
2021-04-19 02:54:07 +0200

    First commit

---
+++ mobile-web-app/src/main/java/com/sergeyk/course/ws/MobileWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.sergeyk.course.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobileWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobileWebAppApplication.class, args);
+	}
+
+}
---
+++ mobile-web-app/src/test/java/com/sergeyk/course/ws/MobileWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sergeyk.course.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobileWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
