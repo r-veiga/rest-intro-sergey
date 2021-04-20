e87c82068b0c40e3c94a984409797c2d73992243
vero9003 <Roberto.VeigaBorneo@nielsen.com>
2021-04-20 22:54:06 +0200

    GET /users accepts request params /users?page=3...

--- mobile-web-app/src/main/java/com/sergeyk/course/ws/ui/controller/UserController.java
+++ mobile-web-app/src/main/java/com/sergeyk/course/ws/ui/controller/UserController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("users")
 public class UserController {
 
+    @GetMapping
+    public String getUser(@RequestParam(value="page") int page, @RequestParam(value="limit") int limit) {
+        return "get user was called with page = " + page + " and limit = " + limit;
+    }
+
     @GetMapping(path="/{userId}")
     public String getUser(@PathVariable String userId) {
-        return "get user was called with parameter " + userId;
+        return "get user was called with path parameter " + userId;
     }
 
     @PostMapping
